e4e741281d0deb782c0f6cef611d1fa79915ef17
mohamadsalhani <mim25@mail.aub.edu>
2022-08-17 15:14:38 +0400

    fixed sql file for report Cards Tracking

--- Old Report Cards Tracking/Cards Report LiveReport.sql
+++ Old Report Cards Tracking/Cards Report LiveReport.sql
@@ -1,7 +1,7 @@
 SELECT * FROM cards_tracking
 WHERE 
 performer LIKE '%'+N%1+'%'
-and reference_number LIKE '%'+N%2+'%'
+and reference_number LIKE N%2+'%'
 and from_status LIKE '%'+N%3+'%'
 and to_status LIKE '%'+N%4+'%'
 and created_at between %5 and %6+1
