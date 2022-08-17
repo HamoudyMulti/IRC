ec6e18fe35df40e03e178c2cd6be7c8457c8a888
mohamadsalhani <mim25@mail.aub.edu>
2022-08-17 04:01:18 +0400

    report updated and ready for new table

--- Old Report Cards Tracking/Cards Report LiveReport.sql
+++ Old Report Cards Tracking/Cards Report LiveReport.sql
@@ -1,38 +1,7 @@
-SELECT * FROM(Select
-Dtree.CreateDate, 
-OTDBOwner.ValStr(ID,max(VerNum),1126878,49)as 'Reference Number',
-OTDBOwner.ValStr(ID,max(VerNum),1126878,29)as 'Card Number',
-OTDBOwner.ValStr(ID,max(VerNum),1126878,26)as 'Delivery Status',
-OTDBOwner.ValStr(ID,max(VerNum),1126878,24)as 'Card Type',
-OTDBOwner.ValStr(ID,max(VerNum),1126878,6)as 'Card State',
-OTDBOwner.ValStr(ID,max(VerNum),1126878,27)as 'Card Activated',
-ID
-from OTDBOwner.LLAttrData WITH (NOLOCK),OTDBOwner.DTree WITH (NOLOCK) WHERE DefID in (1126878)
-and DTree.DataID=LLAttrData.ID
-and DTree.SubType=848 and Name <> 'Card'
-group by ID,DTree.CreateDate,DTree.CreatedBy
-
-UNION ALL
-
-Select 
-Dtree.CreateDate,
-OTDBOwner.ValStr(ID,max(VerNum),1293752,7)as 'Reference Number',
-NULL AS 'Card Number',
-OTDBOwner.ValStr(ID,max(VerNum),1293752,4)as 'Delivery Status',
-NULL AS 'Card Type',
-NULL AS 'Card State',
-NULL as 'Card Activated',
-ID
-FROM OTDBOwner.LLAttrData WITH (NOLOCK),OTDBOwner.DTree WITH (NOLOCK) WHERE DefID in (1293752)
-and DTree.DataID=LLAttrData.ID
-and DTree.SubType=848 and Name <> 'PIN'
-group by ID,DTree.CreateDate,DTree.CreatedBy) AS C
-
-
-WHERE C.[Reference Number] LIKE N%1+'%'
-and C.[Card Number] LIKE N%2+'%'
-and C.[Delivery Status] LIKE N%3+'%'
-and C.[Card Type] LIKE N%4+'%'
-and C.[Card State] LIKE N%5+'%'
-and C.[Card Activated] LIKE N%6+'%'
-and C.[CreateDate] between %7 and %8+1 
+SELECT * FROM cards_tracking
+WHERE 
+performer LIKE '%'+N%1+'%'
+and reference_number LIKE '%'+N%2+'%'
+and from_status LIKE '%'+N%3+'%'
+and to_status LIKE '%'+N%4+'%'
+and created_at between %5 and %6+1
